ef3df8d2888a173f1008670b6cda6a6b4ae6f73f
YihaoSu <yhsu@astro.ncu.edu.tw>
2016-12-03 16:17:25 +0800

    yhsu: add functionality for installation

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from setuptools import setup, find_packages
 import os
 
 data_dir = os.path.expanduser('~/astro_data')
@@ -6,3 +7,25 @@ if not os.path.exists(data_dir):
     os.mkdir(data_dir)
 else:
     print('The directory "' + data_dir + '" already exists.')
+
+## codes for data exportation
+
+
+##
+
+NAME = 'iancupy_data'
+# VERSION should be PEP440 compatible (https://www.python.org/dev/peps/pep-0440/)
+VERSION = '0.0.dev1'
+
+setup(name=NAME,
+      version=VERSION,
+      description='Data preparation for IANCU Python exercise',
+      install_requires=['astropy', 'astroquery'],
+      author='Cheng-Chung Chen, PoShih Chiang, Yi-Hao Su',
+      author_email='yhsu@astro.ncu.edu.tw',
+      license='MIT',
+      packages=find_packages(),
+      url='https://github.com/Astrohackers-TW/IANCUPyData.git',
+      long_description='',
+      zip_safe=False,
+)
