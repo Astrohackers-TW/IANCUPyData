714e461f11f353d6bae578563029c624df384f94
YihaoSu <yhsu@astro.ncu.edu.tw>
2017-03-19 16:04:18 +0800

    yhsu: fix File already exists error when output M4_tmc.fits

--- iancupy_data/download_file.py
+++ iancupy_data/download_file.py
@@ -99,4 +99,4 @@ def get_m4():
 
     cols = fits.ColDefs([col1, col2, col3, col4, col5, col6, col7, col8])
     tmc_hdu = fits.BinTableHDU.from_columns(cols)
-    tmc_hdu.writeto(data_dir + target + '_tmc.fits')
+    tmc_hdu.writeto(data_dir + target + '_tmc.fits', overwrite=True)
